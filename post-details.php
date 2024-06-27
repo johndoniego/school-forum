5819346d2eaf16c9d47b3606ae7b85c4c017a89e
crimera <44558091+crimera@users.noreply.github.com>
2024-06-27 11:38:30 +0800

    fix: header styling

--- post-details.php
+++ post-details.php
@@ -11,6 +11,7 @@ $query = "SELECT * FROM posts WHERE PostID = ?";
 $stmt = $conn->prepare($query);
 $stmt->bind_param("i", $postId);
 $stmt->execute();
+
 $result = $stmt->get_result();
 if ($result->num_rows > 0) {
     $post = $result->fetch_assoc();
@@ -20,20 +21,25 @@ if ($result->num_rows > 0) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($post['title']) ?></title>
     <link rel="stylesheet" href="assets/bootstrap-5.3.0-alpha3-dist/css/bootstrap.css">
-    <!-- <link rel="stylesheet" href="css/sidebar.css"> -->
-    <!-- <link rel="stylesheet" href="css/header.css"> -->
+
+    <link rel="stylesheet" href="css/sidebar.css">
+    <link rel="stylesheet" href="css/header.css">
 </head>
+
 <body>
-<?php include('commons/sidebar.php')?>
-<?php include('commons/header.php')?>
-    <div class="container mt-5">
-        <h1><?= ($post['Title']) ?></h1>
+    <?php include('commons/sidebar.php') ?>
+    <?php include('commons/header.php') ?>
+
+    <div class="container mt-5" style="border:1px solid black;">
+        <h1><?= $post['Title'] ?></h1>
         <p><?= nl2br($post['Content']) ?></p>
     </div>
 </body>
+
 </html>
